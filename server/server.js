0cd00c392a738d912545f8484c04ade6ddeb76bf
subuchan <112760869+subuchan@users.noreply.github.com>
2025-06-25 01:08:26 +0530

    server.js

--- server/server.js
+++ server/server.js
@@ -754,5 +754,5 @@ app.post("/api/us-apply", async (req, res) => {
 
 // ✅ Start server
 app.listen(5000, () => {
-  console.log("Server running on http://localhost:5000");
+  console.log("Server running on render");
 });
